2c95dfa1259a0ebe335b8eb14073501d87db0361
Yiqian Zhou <yiqian@lightup.ai>
2020-12-09 12:51:56 -0800

    Add source command

--- lightctl/command/common.py
+++ lightctl/command/common.py
@@ -2,5 +2,7 @@ from lightctl.util import CliPrinter
 
 
 class ContextObject:
+    """ an object that pass to child command """
+
     def __init__(self, printer: CliPrinter):
         self.printer = printer

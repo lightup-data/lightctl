9f6299c907236650659e3d2e6ca05b1939a0e06f
Yiqian Zhou <yiqian@lightup.ai>
2020-12-09 11:44:18 -0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+# Usage: source dev.sh
+
+# go to right folder
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${REPO_DIR}" || exit
+
+# exit previous venv if any
+deactivate || true
+
 # install pre-commit, use it Black as in .pre-commit-config.yaml
 pip3 install pre-commit
 pre-commit install
 
 # set python dev env
+python3 -m venv lightctl-venv
+source lightctl-venv/bin/activate
 pip3 install -r requirements.txt
 export PYTHONPATH=$(pwd):$PYTHONPATH

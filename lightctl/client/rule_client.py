0ea3da574549266c837a602467673f380bb6f190
rajiv ramanathan <rajiv@lightup.ai>
2021-05-13 15:36:30 -0700

    add support for incident client

--- lightctl/client/rule_client.py
+++ lightctl/client/rule_client.py
@@ -1,5 +1,6 @@
 import logging
 import urllib.parse
+from typing import List, Optional
 
 from lightctl.client.base_client import BaseClient
 from lightctl.config import API_VERSION
@@ -33,3 +34,18 @@ class RuleClient(BaseClient):
 
     def delete_rule(self, id):
         self.delete(self.rules_url, id)
+
+    def get_rules_by_metric(self, metric_uuid) -> List[dict]:
+        rules_in_metric = []
+
+        rules = self.list_rules()
+        for rule in rules:
+            if metric_uuid in rule["config"]["metrics"]:
+                rules_in_metric.append(rule)
+        return rules_in_metric
+
+    def last_processed_timestamp(self, id) -> Optional[float]:
+        rule = self.get_rule(id)
+        if rule is None:
+            return None
+        return rule["status"]["lastSampleTs"]

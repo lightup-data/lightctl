3b0cc2f75663fda529a9dc5593b3152059e3f200
rajiv ramanathan <rajiv@lightup.ai>
2021-09-19 23:05:53 -0700

    lightctl: update to include cleanup, use query parameters to make efficient requests

--- lightctl/client/healthz_client.py
+++ lightctl/client/healthz_client.py
@@ -1,5 +1,6 @@
 import logging
 import urllib.parse
+from typing import Dict
 
 from lightctl.client.base_client import BaseClient
 from lightctl.config import API_VERSION
@@ -9,8 +10,8 @@ logger = logging.getLogger(__name__)
 
 class HealthzClient(BaseClient):
     @property
-    def healthz_url(self):
+    def healthz_url(self) -> str:
         return urllib.parse.urljoin(self.url_base, f"/api/{API_VERSION}/healthz/")
 
-    def get_healthz_info(self):
+    def get_healthz_info(self) -> Dict:
         return self.get(self.healthz_url)
--- lightctl/client/rule_client.py
+++ lightctl/client/rule_client.py
@@ -1,6 +1,6 @@
 import logging
 import urllib.parse
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from lightctl.client.base_client import BaseClient
 from lightctl.config import API_VERSION
@@ -10,46 +10,36 @@ logger = logging.getLogger(__name__)
 
 class RuleClient(BaseClient):
     @property
-    def rules_url(self):
+    def rules_url(self) -> str:
         return urllib.parse.urljoin(self.url_base, f"/api/{API_VERSION}/rules/")
 
-    def list_rules(self):
+    def list_rules(self) -> List[Dict]:
         res = self.get(self.rules_url)
         return res.get("data", [])
 
-    def get_rule(self, id: str):
+    def get_rule(self, id: str) -> Dict:
         url = urllib.parse.urljoin(self.rules_url, id)
         rule = self.get(url)
         return rule
 
-    # TODO: [v1] support query in backend
-    def get_rule_by_name(self, name):
+    def get_rule_by_name(self, name) -> List[Dict]:
+        url = urllib.parse.urljoin(self.rules_url, f"?names={name}")
         # name is not unique
-        rule_list = []
-        rules = self.list_rules()
-        for rule in rules:
-            if rule["name"] == name:
-                rule_list.append(rule)
-        return rule_list
+        return self.get(url)
 
-    def create_rule(self, data):
+    def create_rule(self, data) -> Dict:
         return self.post(self.rules_url, data)
 
-    def update_rule(self, id, data):
+    def update_rule(self, id, data) -> Dict:
         url = urllib.parse.urljoin(self.rules_url, id)
         return self.put(url, data)
 
     def delete_rule(self, id):
         self.delete(self.rules_url, id)
 
-    def get_rules_by_metric(self, metric_uuid) -> List[dict]:
-        rules_in_metric = []
-
-        rules = self.list_rules()
-        for rule in rules:
-            if metric_uuid in rule["config"]["metrics"]:
-                rules_in_metric.append(rule)
-        return rules_in_metric
+    def get_rules_by_metric(self, metric_uuid) -> List[Dict]:
+        url = urllib.parse.urljoin(self.rules_url, f"?metric_uuids={metric_uuid}")
+        return self.get(url)
 
     def last_processed_timestamp(self, id) -> Optional[float]:
         rule = self.get_rule(id)

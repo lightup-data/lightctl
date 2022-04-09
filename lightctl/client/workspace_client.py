809ec3f36ea64750a6283d6c94d1f6b2eca3134b
Rajiv Ramanathan <rajiv@lightup.ai>
2022-04-08 17:19:23 -0700

    updates based on code review

--- lightctl/client/workspace_client.py
+++ lightctl/client/workspace_client.py
@@ -8,17 +8,17 @@ logger = logging.getLogger(__name__)
 
 
 class WorkspaceClient(BaseClient):
-    def workspaces_url(self, workspace_id) -> str:
+    def workspaces_url(self) -> str:
         return urllib.parse.urljoin(self.url_base, f"/api/v1//workspaces/")
 
-    def create_workspace(self, workspace_id: str, name: str):
-        url = self.workspaces_url(workspace_id)
-        payload = {"name": name, "uuid": workspace_id}
+    def create_workspace(self, name: str):
+        url = self.workspaces_url()
+        payload = {"name": name}
         return self.post(url, payload)
 
-    def delete_workspace(self, workspace_id: str, force=False) -> Dict:
-        url = urllib.parse.urljoin(self.workspaces_url(workspace_id))
-        return self.delete(url, workspace_id, force)
+    def delete_workspace(self, workspace_id: str) -> Dict:
+        url = urllib.parse.urljoin(self.workspaces_url)
+        return self.delete(url, workspace_id)
 
-    def list_workspaces(self, workspace_id: str) -> List[Dict]:
-        return self.get(self.workspaces_url(workspace_id))
+    def list_workspaces(self) -> List[Dict]:
+        return self.get(self.workspaces_url())

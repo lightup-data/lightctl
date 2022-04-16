cd0c8b82f658151f325049ec7d84ba4654d7a654
Rajiv Ramanathan <rajiv@lightup.ai>
2022-04-15 23:20:05 -0700

    lightctl: remove unused commands and add help text
    bump version to 0.9.0

--- lightctl/command/source_command.py
+++ lightctl/command/source_command.py
@@ -8,13 +8,16 @@ source_client = SourceClient()
 @click.group()
 def source():
     """
-    used by commands below
+    configure and query lightup datasources
     """
 
 
 @source.command()
 @click.pass_obj
 def list(context_obj):
+    """
+    list all datasources configurations in a workspace
+    """
     res = source_client.list_sources(context_obj.workspace_id)
     context_obj.printer.print(res)
 
@@ -23,6 +26,9 @@ def list(context_obj):
 @click.argument("id")
 @click.pass_obj
 def get(context_obj, id):
+    """
+    list datasource configuration for specified uuid in a workspace
+    """
     res = source_client.get_source(context_obj.workspace_id, id)
     context_obj.printer.print(res)
 
@@ -31,6 +37,9 @@ def get(context_obj, id):
 @click.argument("id")
 @click.pass_obj
 def delete(context_obj, id):
+    """
+    delete datasource based on specified uuid
+    """
     res = source_client.delete_source(context_obj.workspace_id, id)
     context_obj.printer.print(res)
 
@@ -38,22 +47,10 @@ def delete(context_obj, id):
 @source.command()
 @click.argument("id")
 @click.pass_obj
-def clone(context_obj, id):
-    res = source_client.get_source(context_obj.workspace_id, id)
-    if not res:
-        context_obj.printer.print({"error": "not found"})
-        return
-
-    res["metadata"].pop("uuid")
-    res["metadata"]["name"] += "_Clone"
-    res = source_client.create_source(context_obj.workspace_id, res)
-    context_obj.printer.print(res)
-
-
-@source.command()
-@click.argument("id")
-@click.pass_obj
 def trigger(context_obj, id):
+    """
+    trigger a datasource scan, if datasource is configured as triggered
+    """
     res = source_client.get_source(context_obj.workspace_id, id)
     if not res:
         context_obj.printer.print({"error": "not found"})
@@ -66,6 +63,9 @@ def trigger(context_obj, id):
 @click.argument("file", type=click.Path(exists=True))
 @click.pass_obj
 def create(context_obj, file):
+    """
+    create a datasource in the workspace from json or yaml file
+    """
     data = context_obj.file_loader.load(file)
     res = source_client.create_source(context_obj.workspace_id, data)
     context_obj.printer.print(res)
@@ -76,6 +76,9 @@ def create(context_obj, file):
 @click.argument("file", type=click.Path(exists=True))
 @click.pass_obj
 def update(context_obj, id, file):
+    """
+    update datasource in the workspace from json or yaml file
+    """
     data = context_obj.file_loader.load(file)
     res = source_client.update_source(context_obj.workspace_id, id, data)
     context_obj.printer.print(res)
@@ -85,6 +88,9 @@ def update(context_obj, id, file):
 @click.argument("file", type=click.Path(exists=True))
 @click.pass_obj
 def inspect(context_obj, file):
+    """
+    test datasource configuration validity without saving it
+    """
     data = context_obj.file_loader.load(file)
     res = source_client.inspect(context_obj.workspace_id, data)
     context_obj.printer.print(res)
@@ -94,6 +100,9 @@ def inspect(context_obj, file):
 @click.argument("id")
 @click.pass_obj
 def list_tables(context_obj, id):
+    """
+    list all tables in a datasource
+    """
     res = source_client.list_tables(context_obj.workspace_id, id)
     context_obj.printer.print(res)
 
@@ -102,6 +111,9 @@ def list_tables(context_obj, id):
 @click.argument("id")
 @click.argument("table_name")
 @click.pass_obj
-def get_schema(context_obj, id, table_name):
+def get_table_schema(context_obj, id, table_name):
+    """
+    get schema for a table in a datasource
+    """
     res = source_client.get_schema(context_obj.workspace_id, id, table_name)
     context_obj.printer.print(res)

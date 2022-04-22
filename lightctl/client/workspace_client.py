ce49311b2d26b4872805cf942f92b7acd0056bee
sergeybaranov <sergey@lightup.ai>
2022-04-22 09:48:18 -0700

    delete workspace

--- lightctl/client/workspace_client.py
+++ lightctl/client/workspace_client.py
@@ -11,22 +11,15 @@ class WorkspaceClient(BaseClient):
     def workspaces_url(self) -> str:
         return urllib.parse.urljoin(self.url_base, f"/api/v1/workspaces/")
 
-    def deactivate_workspace_url(self, workspace_id) -> str:
-        return urllib.parse.urljoin(self.url_base, f"/api/v1/ws/{workspace_id}/workspaces/deactivate")
-
     def create_workspace(self, name: str):
         url = self.workspaces_url()
         payload = {"name": name}
         res = self.post(url, payload)
         return res["data"]
 
-    def deactivate_workspace(self, workspace_id: str):
-        url = self.deactivate_workspace_url(workspace_id)
-        return self.post(url, {}, expected_status=204)
-
-    # def delete_workspace(self, workspace_id: str) -> Dict:
-    #     url = urllib.parse.urljoin(self.workspaces_url)
-    #     return self.delete(url, workspace_id)
+    def delete_workspace(self, workspace_id: str) -> Dict:
+        url = self.workspaces_url()
+        return self.delete(url, workspace_id)
 
     def list_workspaces(self) -> List[Dict]:
         res = self.get(self.workspaces_url())

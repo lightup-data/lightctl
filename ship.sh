809d3aebc8d3b816ad5121d44a978d53c9ed316e
Rajiv Ramanathan <rajiv@lightup.ai>
2022-10-30 18:53:20 -0700

    Update license file (#51)

--- ship.sh
+++ ship.sh
@@ -3,16 +3,17 @@
 # Usage: ./ship.sh
 
 # Requirement
-# - we are using s3pypi that use S3 to distribute python package. See https://github.com/novemberfiveco/s3pypi
+# - we are using s3pypi that use S3 to distribute python package.
+#   See https://github.com/novemberfiveco/s3pypi
 # - bucket is managed by aurora/terraform
 # - pip install s3pypi
 
 BUCKET=pypi.lightup.ai
-SECRET=poc
+FOLDER=poc
 
 printf "This script package lightctl and upload to s3\n"
 printf "To install, use \n"
-printf "pip install lightctl --find-links https://s3-us-west-2.amazonaws.com/%s/%s/lightctl/index.html\n" ${BUCKET} ${SECRET}
+printf "pip install lightctl --find-links https://s3-us-west-2.amazonaws.com/%s/%s/lightctl/index.html\n" ${BUCKET} ${FOLDER}
 
 printf "Uploading package... \n"
-s3pypi --private --bucket ${BUCKET} --secret ${SECRET} "$@"
+s3pypi --private --bucket ${BUCKET} --secret ${FOLDER} "$@"

ed00d9662e199391f3e14fab32a16110d80e3d07
Rajiv Ramanathan <rajiv@lightup.ai>
2022-11-01 10:38:38 -0700

    healthz api uses v0 version

--- lightctl/client/healthz_client.py
+++ lightctl/client/healthz_client.py
@@ -3,7 +3,6 @@ import urllib.parse
 from typing import Dict
 
 from lightctl.client.base_client import BaseClient
-from lightctl.config import API_VERSION
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +10,7 @@ logger = logging.getLogger(__name__)
 class HealthzClient(BaseClient):
     @property
     def healthz_url(self) -> str:
-        return urllib.parse.urljoin(self.url_base, f"/api/{API_VERSION}/healthz/")
+        return urllib.parse.urljoin(self.url_base, f"/api/v0/healthz/")
 
     def get_healthz_info(self) -> Dict:
         return self.get(self.healthz_url)

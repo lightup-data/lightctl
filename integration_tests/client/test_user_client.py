854705c17e9dfc8161e394dd7c2bfa42ecadb0bb
Yi Cui <yi@lightup.ai>
2023-09-21 12:42:53 -0700

    lightctl: Add app user detail patch in lightctl (#56)
    * lightctl: Add app user detail patch in lightctl
    
    * Update test_user_client.py
    
    * Update test_user_client.py

--- integration_tests/client/test_user_client.py
+++ integration_tests/client/test_user_client.py
@@ -4,8 +4,10 @@ from typing import Dict
 import pytest
 
 from lightctl.client.user_client import UserClient
+from lightctl.util import LightupException
 
 TEST_EMAIL_ADDRESS = "fillmein+{}@xyz.com"
+
 
 class TestUserClient:
     @pytest.fixture
@@ -71,3 +73,24 @@ class TestUserClient:
 
         res = fixture_user_client.list_users(workspace_id)
         assert len(res) == 0
+
+    def test_update_app_user_detail(
+        self,
+        fixture_user_client: UserClient,
+    ):
+        user_id = TEST_EMAIL_ADDRESS
+        try:
+            fixture_user_client.delete_app_user(user_id)
+        except LightupException:
+            pass
+        fixture_user_client.add_app_user(user_id, "app_viewer")
+        app_user = fixture_user_client.get_app_user(user_id)
+        assert app_user["expiration_timestamp"] is None
+
+        fixture_user_client.update_app_user_detail(user_id, {"expirationTimestamp": 1704067200})
+
+        app_user = fixture_user_client.get_app_user(user_id)
+        assert app_user["expiration_timestamp"] == 1704067200
+
+        # clean up
+        fixture_user_client.delete_app_user(user_id)

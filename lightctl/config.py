b0e10b088d7f0d37cdedcf00d002091a39c705a3
Rajiv Ramanathan <rajiv@lightup.ai>
2024-03-12 09:47:16 -0700

    Update env variables to start with LIGHTCTL_ to prevent collision (#66)
    * bump pre-commit versions using pre-commit autoupdate
    
    * rename variable to match the environment variable, prepend with LIGHTCTL to indicate owner
    
    * bump env variable
    
    * update base workspace

--- lightctl/config.py
+++ lightctl/config.py
@@ -3,16 +3,16 @@ from pathlib import Path
 
 API_VERSION = "v1"
 
-ACCESS_TOKEN_CACHE_FILE_PATH = os.environ.get(
-    "CACHED_CREDENTIAL_PATH",
+LIGHTCTL_TOKEN_CACHE_PATH = os.environ.get(
+    "LIGHTCTL_TOKEN_CACHE_PATH",
     os.path.join(str(Path.home()), ".lightup", ".access-token-cache"),
 )
 
-CREDENTIAL_FILE_PATH = os.environ.get(
+LIGHTCTL_CREDENTIAL_PATH = os.environ.get(
     "LIGHTCTL_CREDENTIAL_PATH",
     os.path.join(str(Path.home()), ".lightup", "credential"),
 )
 
-BASE_WORKSPACE_UUID = os.environ.get(
+LIGHTCTL_DEFAULT_WORKSPACE = os.environ.get(
     "LIGHTCTL_DEFAULT_WORKSPACE", "497d2c3e-2e24-47ec-b33a-dcf3999062a7"
 )
